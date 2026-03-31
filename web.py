2cc1efce720d6b04edf956f9c1ad6ca152148429
楊子青 (Tzyy-Ching Yang) <tcyang@gm.pu.edu.tw>
2026-03-31 14:25:19 +0800

    web

---
+++ web.py
@@ -0,0 +1,79 @@
+from flask import Flask, render_template, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    link = "<h1>歡迎進入楊子青的網站首頁</h1>"
+    link += "<a href=/mis>課程</a><hr>"
+    link += "<a href=/today>今天日期</a><hr>"
+    link += "<a href=/about>關於子青</a><hr>"
+    link += "<a href=/welcome?u=子青&dep=靜宜資管>GET傳值</a><hr>"   
+    link += "<a href=/account>POST傳值(帳號密碼)</a><hr>" 
+    link += "<a href=/math>數學運算</a><hr>" 
+    return link
+
+@app.route("/mis")
+def course():
+    return "<h1>資訊管理導論</h1><a href=/>回到網站首頁</a>"
+
+@app.route("/today")
+def today():
+    now = datetime.now()
+    year  = str(now.year)   # 取得年份 
+    month = str(now.month)  # 取得月份 
+    day   = str(now.day)    # 取得日期 
+    now = year + "年" + month + "月" + day + "日"
+    return render_template("today.html", datetime = now)
+
+@app.route("/about")
+def about():
+    return render_template("mis2a.html")
+
+@app.route("/welcome", methods=["GET"])
+def welcome():
+    x = request.values.get("u")
+    y = request.values.get("dep")
+    return render_template("welcome.html", name = x, dep = y)
+
+@app.route("/account", methods=["GET", "POST"])
+def account():
+    if request.method == "POST":
+        user = request.form["user"]
+        pwd = request.form["pwd"]
+        result = "您輸入的帳號是：" + user + "; 密碼為：" + pwd 
+        return result
+    else:
+        return render_template("account.html")
+
+@app.route("/math", methods=["GET", "POST"])
+def math():
+    if request.method == "POST":
+        x = int(request.form["x"])
+        opt = request.form["opt"]
+        y = int(request.form["y"])      
+        result = "您輸入的是：" + str(x) + opt + str(y)
+        
+        if (opt == "/" and y == 0):
+            result += "，除數不能為0"
+        else:
+            match opt:
+                case "+":
+                    r = x + y
+                case "-":
+                    r = x - y
+                case "*":
+                    r = x * y
+                case "/":
+                    r = x / y  # 修正：之前誤寫為 x - y
+                case _:
+                    return "未知運算符號"
+            result += "=" + str(r)  + "<br><a href=/>返回首頁</a>"          
+        return result
+    else:
+        return render_template("math.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
