e1b557b321a671dcffd69a0ce8c01d72c7db1d92
楊子青 (Tzyy-Ching Yang) <tcyang@gm.pu.edu.tw>
2026-04-09 11:03:57 +0800

    0409a

--- web.py
+++ web.py
@@ -1,43 +1,38 @@
 from flask import Flask, render_template, request
 from datetime import datetime
-import random
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    link = "<h1>歡迎進入楊子青的網站首頁</h1>"
+    link = "<h1>歡迎進入楊子青的網站20260409</h1>"
     link += "<a href=/mis>課程</a><hr>"
-    link += "<a href=/today>今天日期</a><hr>"
-    link += "<a href=/about>關於子青</a><hr>"
-    link += "<a href=/welcome?u=子青&dep=靜宜資管>GET傳值</a><hr>"   
-    link += "<a href=/account>POST傳值(帳號密碼)</a><hr>" 
-    link += "<a href=/math>數學運算</a><hr>" 
-    link += "<a href=/cup>擲茭</a><hr>"
+    link += "<a href=/today>現在日期時間</a><hr>"
+    link += "<a href=/me>關於我</a><hr>"
+    link += "<a href=/welcome?u=子青&d=靜宜資管&c=資訊管理導論>Get傳值</a><hr>"
+    link += "<a href=/account>POST傳值</a><hr>"
     return link
 
 @app.route("/mis")
 def course():
-    return "<h1>資訊管理導論</h1><a href=/>回到網站首頁</a>"
+    return "<h1>資訊管理導論</h1><a href=/>返回首頁</a>"
 
 @app.route("/today")
 def today():
     now = datetime.now()
-    year  = str(now.year)   # 取得年份 
-    month = str(now.month)  # 取得月份 
-    day   = str(now.day)    # 取得日期 
-    now = year + "年" + month + "月" + day + "日"
-    return render_template("today.html", datetime = now)
+    return render_template("today.html", datetime=str(now))
 
-@app.route("/about")
-def about():
-    return render_template("mis2a.html")
+@app.route("/me")
+def me():
+    return render_template("mis2026b.html")
 
 @app.route("/welcome", methods=["GET"])
 def welcome():
-    x = request.values.get("u")
-    y = request.values.get("dep")
-    return render_template("welcome.html", name = x, dep = y)
+    user = request.values.get("u")
+    d = request.values.get("d")
+    c = request.values.get("c")    
+    return render_template("welcome.html", name= user, dep = d, course = c)
+
 
 @app.route("/account", methods=["GET", "POST"])
 def account():
@@ -48,61 +43,6 @@ def account():
         return result
     else:
         return render_template("account.html")
-
-@app.route("/math", methods=["GET", "POST"])
-def math():
-    if request.method == "POST":
-        x = int(request.form["x"])
-        opt = request.form["opt"]
-        y = int(request.form["y"])      
-        result = "您輸入的是：" + str(x) + opt + str(y)
-        
-        if (opt == "/" and y == 0):
-            result += "，除數不能為0"
-        else:
-            match opt:
-                case "+":
-                    r = x + y
-                case "-":
-                    r = x - y
-                case "*":
-                    r = x * y
-                case "/":
-                    r = x / y  # 修正：之前誤寫為 x - y
-                case _:
-                    return "未知運算符號"
-            result += "=" + str(r)  + "<br><a href=/>返回首頁</a>"          
-        return result
-    else:
-        return render_template("math.html")
-
-@app.route('/cup', methods=["GET"])
-def cup():
-    # 檢查網址是否有 ?action=toss
-    #action = request.args.get('action')
-    action = request.values.get("action")
-    result = None
-    
-    if action == 'toss':
-        # 0 代表陽面，1 代表陰面
-        x1 = random.randint(0, 1)
-        x2 = random.randint(0, 1)
-        
-        # 判斷結果文字
-        if x1 != x2:
-            msg = "聖筊：表示神明允許、同意，或行事會順利。"
-        elif x1 == 0:
-            msg = "笑筊：表示神明一笑、不解，或者考慮中，行事狀況不明。"
-        else:
-            msg = "陰筊：表示神明否定、憤怒，或者不宜行事。"
-            
-        result = {
-            "cup1": "/static/" + str(x1) + ".jpg",
-            "cup2": "/static/" + str(x2) + ".jpg",
-            "message": msg
-        }
-        
-    return render_template('cup.html', result=result)
 
 
 if __name__ == "__main__":
