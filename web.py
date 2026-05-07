9e88de17db11480b6f502b0b9ae0ea080742eed7
楊子青 (Tzyy-Ching Yang) <tcyang@gm.pu.edu.tw>
2026-05-07 10:59:52 +0800

    open data 2

--- web.py
+++ web.py
@@ -60,7 +60,7 @@ def road():
 
     JsonData = json.loads(Data.text)
     for item in JsonData:
-        R += item["路口名稱"] + ",原因:" + item["主要肇因"] + "<br>"
+        R += item["路口名稱"] + ",原因:" + item["主要肇因"] + ",件數:" + item["總件數"] + "<br>"
 
     return R
 
