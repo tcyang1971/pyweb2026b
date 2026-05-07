88467182ecd837bc2cf1a32b53f0113d7f77d734
楊子青 (Tzyy-Ching Yang) <tcyang@gm.pu.edu.tw>
2026-05-07 11:05:24 +0800

    open data 3

--- web.py
+++ web.py
@@ -42,7 +42,7 @@ def index():
 
 @app.route("/road")
 def road():
-    R = "<h1>台中市十大肇事路口(113年10月)</h1><br>"
+    R = "<h1>台中市十大肇事路口(113年10月) 作者:楊子青</h1><br>"
 
     url = "https://datacenter.taichung.gov.tw/swagger/OpenData/a1b899c0-511f-4e3d-b22b-814982a97e41"
 
